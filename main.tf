7a44a4ed8037607120364c6a6a0f8630b58e126d
Oloruntobi Olurombi <oloruntobiolurombi@gmail.com>
2024-08-27 12:23:36 +0100

    install tools and packages

--- main.tf
+++ main.tf
@@ -90,3 +90,11 @@ resource "aws_eks_node_group" "eks_node_group" {
 output "endpoint" {
   value = aws_eks_cluster.eks_cluster.endpoint
 }
+
+output "ec2_public_ip" {
+    value = aws_instance.control_plane.public_ip 
+}
+
+output "eks_cluster_name" {
+    value = aws_eks_cluster.eks_cluster.name
+}
