3fc044e0e38bcf5ff14b090a059671f07f31689b
Oloruntobi Olurombi <oloruntobiolurombi@gmail.com>
2024-08-29 16:31:36 +0100

    adjust workflow

--- main.tf
+++ main.tf
@@ -95,6 +95,10 @@ output "ec2_public_ip" {
     value = aws_instance.control_plane.public_ip 
 }
 
+output "ec2_instance_id" {
+    value = aws_instance.control_plane.id
+}
+
 output "eks_cluster_name" {
     value = aws_eks_cluster.eks_cluster.name
 }
